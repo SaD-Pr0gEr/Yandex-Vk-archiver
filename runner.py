e67418e53af91f25cdcdfd0aa993c6c836d6a1b4
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-12-05 17:21:28 +0500

    Project restructure(RESTART PROJECT)

--- runner.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    from app.config import TOKEN, VERSION, User_ID, TOKEN_YANDEX
-    from app.parser import VkParser
-    from app.uploader import YandexUpload
-
-    vk_photos = VkParser(TOKEN, VERSION, User_ID, 5)
-    photo_links_list = vk_photos.get_photo_links()
-    upload_photo = YandexUpload()
-    upload_photo.upload("file_path", TOKEN_YANDEX, photo_links_list)
