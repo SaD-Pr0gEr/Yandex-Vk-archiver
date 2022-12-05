dabc495f45a7fb6918640b0c7db9ba062671cc0b
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-12-05 17:21:00 +0500

    Project restructure(RESTART PROJECT)

--- app/config.py
+++ app/config.py
@@ -1,15 +1,40 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 from dotenv import load_dotenv
 import os
 
 
-BASE_DIR = Path().resolve()
+BASE_DIR = Path().resolve().parent
 
-load_dotenv(BASE_DIR / ".env")
 
-TOKEN = os.getenv("VK_TOKEN")
-TOKEN_YANDEX = os.getenv("YANDEX_TOKEN")
-API_BASE_URL = "https://api.vk.com/method/"
-User_ID = os.getenv("USER_ID_VK")
-VERSION = os.getenv("VERSION", 5.107)
+@dataclass
+class YandexConfig:
+    CLIENT_ID: str
+    APPLICATION_SECRET: str
+
+
+@dataclass
+class VkConfig:
+    TOKEN: str
+    USER_ID: int
+    API_VERSION: int
+
+
+@dataclass
+class AppConfig:
+    YANDEX_CONFIG: YandexConfig
+    VK_CONFIG: VkConfig
+
+
+def load_config(env_file_path: str) -> AppConfig:
+    load_dotenv(env_file_path)
+    VK_TOKEN = os.getenv("VK_TOKEN")
+    VK_USER_ID = int(os.getenv("USER_ID_VK"))
+    VK_API_VERSION = int(os.getenv("VERSION", 5.107))
+    YANDEX_CLIENT_ID = os.getenv("YANDEX_CLIENT_ID")
+    YANDEX_APP_SECRET = os.getenv("YANDEX_CLIENT_ID")
+    return AppConfig(
+        YandexConfig(YANDEX_CLIENT_ID, YANDEX_APP_SECRET),
+        VkConfig(VK_TOKEN, VK_USER_ID, VK_API_VERSION)
+    )
