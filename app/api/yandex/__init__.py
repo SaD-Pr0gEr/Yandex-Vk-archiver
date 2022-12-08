41bbfcc9cdf76463c044b610e5d74b681cd1dc15
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-12-09 00:07:15 +0500

    allowed imports

---
+++ app/api/yandex/__init__.py
@@ -0,0 +1,5 @@
+from .auth import YandexAuthorize
+from .disk import YandexDiskResources, YandexPublicResourcesAPI, UserDiskInfo
+
+
+__all__ = ("YandexAuthorize", "YandexDiskResources", "YandexPublicResourcesAPI", "UserDiskInfo")
--- app/api/yandex/disk/__init__.py
+++ app/api/yandex/disk/__init__.py
@@ -1,1 +1,8 @@
 """Package for working Yandex Disk API."""
+
+from .public_resources import YandexPublicResourcesAPI
+from .resources import YandexDiskResources
+from .info import UserDiskInfo
+
+
+__all__ = ("YandexPublicResourcesAPI", "YandexDiskResources", "UserDiskInfo")
